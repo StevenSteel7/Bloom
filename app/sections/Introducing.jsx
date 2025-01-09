9ec0943bb37f99da386b3f3202a39c8ac3af57ec
Steven Moses <soggsteven92@gmail.com>
2025-01-09 14:43:22 +0530

    bugs-for-deployment-fixed

--- app/sections/Introducing.jsx
+++ app/sections/Introducing.jsx
@@ -1,5 +1,5 @@
 'use client';
-import splitStringUsingRegex from '@/utils/splitStringUsingRegex'
+import splitStringUsingRegex from '../../utils/splitStringUsingRegex'
 import { motion } from 'framer-motion'
 
 const heading = `Introducing`;
